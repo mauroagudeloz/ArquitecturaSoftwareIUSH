86622b30fd2b17eb1406c09c375951a22a03babd
mauroagudeloz <32796409+mauroagudeloz@users.noreply.github.com>
2020-04-16 22:12:33 -0500

    PatronesDiseñoSoftwareSingleton

---
+++ PatronesDiseñoSoftwareSingleton/EnvioSMS.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatronesDiseñoSoftwareSingleton
+{
+
+
+    public class EnvioSMS
+    {
+        private static EnvioSMS EnviarSMSInstance = null;
+        private EnvioSMS()
+        {
+
+        }
+
+        public static EnvioSMS GetInstance
+        {
+            get
+            {
+                if (EnviarSMSInstance == null)
+                    EnviarSMSInstance = new EnvioSMS();
+
+                return EnviarSMSInstance;
+            }
+        }
+
+        public bool EnviarSMS(string Mensaje)
+        {
+            try
+            {
+                Console.WriteLine(Mensaje);
+
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+
+    }
+
+
+
+
+
+    //Multithreaded Singleton
+    public sealed class Singleton
+    {
+        private static volatile Singleton instance;
+        private static object syncRoot = new Object();
+
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new Singleton();
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+
+
+}
---
+++ PatronesDiseñoSoftwareSingleton/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.Design.Serialization;
+
+namespace PatronesDiseñoSoftwareSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnvioSMS NombreVariable = EnvioSMS.GetInstance;
+            NombreVariable.EnviarSMS("Mauro, Escribir mensaje 1 en consola");
+
+            EnvioSMS NombreVariable2 = EnvioSMS.GetInstance;
+            NombreVariable2.EnviarSMS("Mauro, Escribir mensaje 2 en consola");
+
+            EnvioSMS NombreVariable3 = EnvioSMS.GetInstance;
+            NombreVariable3.EnviarSMS("Mauro, Escribir mensaje 3 en consola");
+
+            EnvioSMS NombreVariable4 = EnvioSMS.GetInstance;
+            NombreVariable4.EnviarSMS("Mauro, Escribir mensaje 4 en consola");
+
+
+            
+            Console.ReadLine();
+        }
+
+
+    }
+}
